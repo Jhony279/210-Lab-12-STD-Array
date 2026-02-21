deadb25f3ae31d70817fa7ff24225371cb777eee
Jhony279 <lordjhony732@gmail.comm>
2026-02-20 22:25:54 -0800

    Changed array data type to double and starting second function

--- main.cpp
+++ main.cpp
@@ -8,35 +8,41 @@ using namespace std;
 const int SIZE = 30;
 const string FILE_LOCATION = "C:\\Users\\lordj\\Downloads\\text.txt";
 
-void populateArray(string, array<string, SIZE>&);
-void sortArray();
+void populateArray(string, array<double, SIZE>&);
+void displayArrayInfo(array<double, SIZE>&);
+void sortArray(array<double, SIZE>&);
 
 /**
  * @brief - Description of main
  * @return 
 */
 int main() {
-    array<string, SIZE> textArray;
-    populateArray(FILE_LOCATION, textArray);
-    cout << "Unsorted array (Raw Data)\n";
-    for (string text : textArray) cout << text << endl;
+    array<double, SIZE> doubleArray;
+
+    populateArray(FILE_LOCATION, doubleArray);
+    displayArrayInfo(doubleArray);
 
     return 0;
 }
 
-void populateArray(string fileLoaction, array<string, SIZE>& textArray){
+void populateArray(string fileLoaction, array<double, SIZE>& doubleArray){
     ifstream inputFile;
     string text;
-    // array<string, SIZE> textArray;
+    
     inputFile.open(fileLoaction);
     if (inputFile.good()){
         int i = 0;
-        while (getline(inputFile, text) && i < SIZE){
-            textArray.at(i) = text;
+        while (getline(inputFile, text) && i < doubleArray.size()){
+            doubleArray.at(i) = stod(text);
             i++;
         }
     } else{
         cout << "File was not found!\n";
         return;
     }
+}
+
+void displayArrayInfo(array<double, SIZE>& doubleArray){
+    cout << "Unsorted array (Raw Data)\n";
+    for (double value : doubleArray) cout << value << endl;
 }
