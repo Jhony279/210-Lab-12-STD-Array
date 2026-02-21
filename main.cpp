1825d90da04df6053c23df0f86227865f6edae4f
Jhony279 <lordjhony732@gmail.comm>
2026-02-20 22:56:47 -0800

    Finished fixing function 1

--- main.cpp
+++ main.cpp
@@ -32,21 +32,27 @@ void populateArray(string fileLoaction, array<double, SIZE>& doubleArray){
     inputFile.open(fileLoaction);
     if (inputFile.good()){
         int i = 0;
-            double nText = stod(text);
+        double nText = 0;
         while (getline(inputFile, text) && i < doubleArray.size()){
+            // If Line is empty, skip to next iteration
             if (text.empty()){
                 continue;
-            } else{
-                try {
-
-                }
             }
+            // If string cant convert to double, skip to next iteration
+            try {
+                nText = stod(text);
+            } catch (const invalid_argument& e) {
+                cout << "Invalid data inside file: " << fileLoaction << endl;
+                cout << " Invalid Data: " << text << endl;
+                continue;
+            }
+            // If line is valid, add to array
             doubleArray.at(i) = nText;
-            cout << i;
+            // cout << i;
             i++;
         }
     } else{
-        cout << "File was not found!\n";
+        cout << "File was not found!"<< endl << endl;
         return;
     }
 }
